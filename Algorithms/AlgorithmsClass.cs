7757586915e7cd4d19ca609991975e5c457833a7
Bassem <52628535+bassemashraf@users.noreply.github.com>
2025-01-02 16:50:21 +0200

    Add project files.

---
+++ Algorithms/AlgorithmsClass.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    static class AlgorithmsClass
+    {
+        public static int[] insertionSort(int[] unsortedList)
+        {
+
+            for (int i = 1; i < unsortedList.Count(); i++)
+            {
+                int key = unsortedList[i];
+                int j = i - 1;
+                while (j >= 0 && unsortedList[j] > key)
+                {
+                    unsortedList[j + 1] = unsortedList[j];
+                    --j;
+                }
+                unsortedList[j + 1] = key;
+            }
+            return unsortedList;
+        }
+        public static void mergeSort(int[] unsortedList, int start, int end)
+        {
+            if (start >= end) return;
+            int midpoint = (end + start) / 2;
+            mergeSort(unsortedList, start, midpoint);
+            mergeSort(unsortedList, midpoint + 1, end);
+            merge(unsortedList, start, end, midpoint);
+
+
+        }
+        public static void merge(int[] unsortedList, int start, int end, int midpoint)
+        {
+            int i, j, k;
+            int leftLength = midpoint - start + 1;
+            int rightLength = end - midpoint;
+
+            int[] leftArray = new int[leftLength];
+            int[] rightArray = new int[rightLength];
+
+            for (i = 0; i < leftLength; i++)
+            {
+                leftArray[i] = unsortedList[start + i];
+
+            }
+            for (j = 0; j < rightLength; j++)
+            {
+                rightArray[j] = unsortedList[midpoint + 1 + j];
+            }
+
+            i = 0;
+            j = 0;
+            k = start;
+            while (i < leftLength && j < rightLength)
+            {
+                if (leftArray[i] <= rightArray[j])
+                {
+                    unsortedList[k] = leftArray[i];
+                    i++;
+                }
+                else
+                {
+                    unsortedList[k] = rightArray[j];
+                    j++;
+                }
+                k++;
+
+            }
+
+            while (i < leftLength)
+            {
+                unsortedList[k] = leftArray[i];
+                i++;
+                k++;
+            }
+            while (j < rightLength)
+            {
+                unsortedList[k] = rightArray[j];
+                j++;
+                k++;
+            }
+
+
+        }
+    }
+}
---
+++ Algorithms/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Algorithms;
+
+
+int[] array = { 8, 65, 9, 7, 3, 5, 54 };
+
+Console.WriteLine(String.Join(", ", array));
+AlgorithmsClass.mergeSort(array, 0, array.Length - 1);
+Console.WriteLine(String.Join(", ", array));
